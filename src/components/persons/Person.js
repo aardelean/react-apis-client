bafc1f2441c5c9cafbfd3b39e0f2d4cf786f7c49
aardelean <alex.ardelean@appdirect.com>
2017-07-20 15:44:55 +0200

    Moved Person to redux

--- src/components/persons/Person.js
+++ src/components/persons/Person.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { ListItem } from 'material-ui/List';
+import { doDeletePerson } from './persons-actions';
 import DeletePersonButton from './DeletePersonButton';
 
 const propTypes = {
   person: PropTypes.object.isRequired,
-  onDelete: PropTypes.func.isRequired,
+  handleDelete: PropTypes.func.isRequired,
 };
+const mapStateToProps = () => ({});
 
-const Person = ({ person = {}, onDelete }) => (
+const mapDispatchToProps = dispatch => ({
+  handleDelete: id => dispatch(doDeletePerson(id)),
+});
+
+const Person = ({ person = {}, handleDelete }) => (
   <div>
     <div style={{ margin: '10px' }}>
       <ListItem >
         <label>{person.firstName + ' ' + person.lastName}</label>
-        <DeletePersonButton onDelete={onDelete} />
+        <DeletePersonButton onDelete={() => handleDelete(person._links.self.href)} />
       </ListItem>
     </div>
   </div>);
 Person.propTypes = propTypes;
 
-export default Person;
+export default connect(mapStateToProps, mapDispatchToProps)(Person);
--- src/components/persons/PersonList.js
+++ src/components/persons/PersonList.js
@@ -3,13 +3,12 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import RaisedButton from 'material-ui/RaisedButton';
 import { List } from 'material-ui/List';
-import { doLoadPerson, doDeletePerson } from './persons-actions';
+import { doLoadPerson } from './persons-actions';
 import Person from './Person';
 
 const propTypes = {
   list: PropTypes.array.isRequired,
   onLoadPerson: PropTypes.func.isRequired,
-  onDelete: PropTypes.func.isRequired,
 };
 
 const getPersons = state => (state.persons.list);
@@ -19,15 +18,13 @@ const mapStateToProps = state => ({
 });
 const mapDispatchToProps = dispatch => ({
   onLoadPerson: () => dispatch(doLoadPerson()),
-  onDelete: id => event => dispatch(doDeletePerson(id)),
 });
 
-const PersonList = ({ list = [], onLoadPerson, onDelete }) => {
+const PersonList = ({ list = [], onLoadPerson }) => {
   const personList = list.map(item => (
     <Person
       key={item._links.self.href}
       person={item}
-      onDelete={onDelete(item._links.self.href)}
     />));
   return (
     <div>
